a2baa09c55b10396a9f5ef63b98a80f986ccaa19
Angel Polanco <angelepolanco325@gmail.com>
2025-11-26 17:28:54 -0500

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
+export default function Navbar() {
+  const { user, logout } = useContext(AuthContext);
+
+  return (
+    <nav className="navbar">
+      <Link to="/">Book Buddy</Link>
+      <div>
+        <Link to="/books">Books</Link>
+        {user ? (
+          <>
+            <Link to="/account">Account</Link>
+            <button onClick={logout}>Logout</button>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </div>
+    </nav>
+  );
+}
