fd4763d3fb5de8d1148f0f598cd1d18a63f47881
Angel Polanco <angelepolanco325@gmail.com>
2025-11-26 17:27:30 -0500

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,38 @@
-export default function App() {
-  return <></>;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { AuthProvider } from "./context/AuthContext";
+import BookList from "./pages/BookList";
+import BookDetails from "./pages/BookDetails";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Account from "./pages/Account";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function App() {
+  return (
+    <Router>
+      <AuthProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<BookList />} />
+          <Route path="/books" element={<BookList />} />
+          <Route path="/books/:id" element={<BookDetails />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/account"
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
+  );
 }
+
+export default App;
